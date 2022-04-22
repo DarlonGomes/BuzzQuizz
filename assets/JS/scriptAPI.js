2dff515d5b90f4a30eabdd54fff8998aea1198fb
Lucas <bazezimo42@gmail.com>
2022-04-22 19:27:33 -0300

    Versão 0.3.9 - Implementação da renderização do quizz aberto

--- assets/JS/scriptAPI.js
+++ assets/JS/scriptAPI.js
@@ -29,23 +29,14 @@ function renderizarQuizzes (el) {
     const containerComunidade = document.querySelector(".container-comunidade");
     const containerUsuario = document.querySelector(".container-usuario");
 
-    console.log(array.length);
-    console.log(el);
-    console.log(el.data[1]);
-    console.log(el.data[1].id);
-    console.log(el.data[1].image);
-    console.log(el.data[1].levels);
-    console.log(el.data[1].questions);
-    console.log(el.data[1].title);
-
-    for(let i = 0; i < array.length; i++) {
-        containerComunidade.innerHTML += `
-        <div class="quizz-comunidade" id="${array[i].id}" onclick="selecionarQuizz(this)" title="${array[i].title}">
-            <img src="${array[i].image}" alt="Imagem do Quizz: ${array[i].title}">
-            <p>${array[i].title}</p>
-        </div>
-        `
-    }
+    // console.log(array.length);
+    // console.log(el);
+    // console.log(el.data[1]);
+    // console.log(el.data[1].id);
+    // console.log(el.data[1].image);
+    // console.log(el.data[1].levels);
+    // console.log(el.data[1].questions);
+    // console.log(el.data[1].title);
 
     for(let i = 0; i < array.length; i++) {
         containerComunidade.innerHTML += `
@@ -64,20 +55,68 @@ function renderizarQuizzes (el) {
 
 function selecionarQuizz(el) {
     idQuizz = el.id;
+    qualPagina = "tela-dois";
+    carregarQuizz();
+}
+
+function carregarQuizz () {
+    document.querySelector(".tela-um").classList.add("escondido");
+    document.querySelector(".tela-dois").classList.add("escondido");
+    document.querySelector(".tela-tres").classList.add("escondido");
+    document.querySelector(".carregando").classList.remove("escondido");
+
     const promisse = axios.get(`${API}/${idQuizz}`);
- 
     promisse.then(renderizarQuizzSelecionado);
 }
 
 function renderizarQuizzSelecionado (el) {
-
     const quizz = el.data;
-    console.log(quizz)
-    console.log(quizz)
-    console.log(quizz.id)
+    const containerQuizz = document.querySelector(".tela-dois");
+    const niveis = quizz.levels.length;
+    const perguntas = quizz.questions.length;
+    
     console.log(quizz)
 
-    
+    containerQuizz.innerHTML = `
+    <div class="banner">
+        <img src="${quizz.image}" alt="Imagem do Quizz: ${quizz.title}">
+        <p>${quizz.title}</p>
+    </div>
+    `
+    for(let i = 0; i < perguntas; i++) {
+        let resposta = quizz.questions[i].answers
+
+        for(let j = 0; j < resposta.length; j++) {
+
+            containerQuizz.innerHTML += `
+            <div class="container-questao">
+                <div class="questao">
+                    <div class="pergunta" style="color:${quizz.questions[i].color}"><p>${quizz.questions[i].title}</p></div>
+                    <div class="resposta">
+                        <img src="${resposta[j].image}" id="${resposta[j].isCorrectAnswer}" alt="Imagem: ${resposta[j].text}"/>
+                        <p>${resposta[j].text}</p>
+                    </div>
+                    <div class="resposta">
+                        <img src="${resposta[j].image}" id="${resposta[j].isCorrectAnswer}" alt="Imagem: ${resposta[j].text}"/>
+                        <p>${resposta[j].text}</p>
+                    </div>
+                    <div class="resposta">
+                        <img src="${resposta[j].image}" id="${resposta[j].isCorrectAnswer}" alt="Imagem: ${resposta[j].text}"/>
+                        <p>${resposta[j].text}</p>
+                    </div>
+                    <div class="resposta">
+                        <img src="${resposta[j].image}" id="${resposta[j].isCorrectAnswer}" alt="Imagem: ${resposta[j].text}"/>
+                        <p>${resposta[j].text}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+
+
+    }
+
+
 
     if(qualPagina === "tela-dois") {
         document.querySelector(".tela-dois").classList.remove("escondido");
@@ -85,9 +124,6 @@ function renderizarQuizzSelecionado (el) {
     }
 }
 
-// <div class="banner">
-// <p>O quão TOP o Lucas é?</p>
-// </div>
 // <div class="container-questao">
 // <div class="questao">
 //     <div class="pergunta"><p>Qual desses Lucas é o mais TOP?</p></div>
