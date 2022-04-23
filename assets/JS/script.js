b965e349242a9efc4f7119e885445ec7d1741324
Lucas <bazezimo42@gmail.com>
2022-04-22 23:47:17 -0300

    Versão 0.4.4 - Logica de validação implementada

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,12 @@
 // VARIÁVEIS GLOBAIS
+let contemErro = 0;
 
 // Variáveis p/ criar quizz (tela 3.1)
 let tituloQuizz = "";
 let urlImagemQuizz = "";
 let qntPerguntasQuizz = "";
 let qntNiveisQuizz = "";
+let novoQuizz = {};
 
 // Variáveis p/ perguntas do quizz (tela 3.2)
 let perguntaTexto = "";
@@ -34,17 +36,81 @@ function criarQuizz() {
 
 //Botão da primeira página de criar quizz, prossegue de página para perguntas e computa os valores da página atual (tela 3.1)
 function prosseguirParaPerguntas() {
-    //pega os inputs
-    tituloQuizz = document.querySelector(".tela-tres-info-quizz .titulo-quizz").value;
-    urlImagemQuizz = document.querySelector(".tela-tres-info-quizz .url-imagem-quizz").value;
-    qntPerguntasQuizz = document.querySelector(".tela-tres-info-quizz .qnt-perguntas-quizz").value;
-    qntNiveisQuizz = document.querySelector(".tela-tres-info-quizz .qnt-niveis-quizz").value;
+    const titulo = document.querySelector(".tela-tres-info-quizz .titulo-quizz");
+    const url = document.querySelector(".tela-tres-info-quizz .url-imagem-quizz");
+    const pergunta = document.querySelector(".tela-tres-info-quizz .qnt-perguntas-quizz");
+    const nivel = document.querySelector(".tela-tres-info-quizz .qnt-niveis-quizz");
 
+    //pega os inputs
+    tituloQuizz = titulo.value;
+    urlImagemQuizz = url.value;
+    qntPerguntasQuizz = pergunta.value;
+    qntNiveisQuizz = nivel.value;
+
+
+    //verifica se as entradas são válida
+    contemErro = 4;
+
+        //Retira excesso de aviso
+    if(titulo.classList.contains("tem-erro")) {
+        titulo.classList.remove("tem-erro");
+        titulo.nextElementSibling.remove();
+    }
+    if(url.classList.contains("tem-erro")) {
+        url.classList.remove("tem-erro");
+        url.nextElementSibling.remove();
+    } 
+    if(nivel.classList.contains("tem-erro")) {
+        nivel.classList.remove("tem-erro");
+        nivel.nextElementSibling.remove();
+    } 
+    if(pergunta.classList.contains("tem-erro")) {
+        pergunta.classList.remove("tem-erro");
+        pergunta.nextElementSibling.remove();
+    }
+
+        //confere erro no título
+    if(tituloQuizz.length > 65 || tituloQuizz.length < 20) {
+        if(!titulo.classList.contains("tem-erro")){
+            titulo.classList.add("tem-erro");
+            titulo.insertAdjacentHTML("afterend",`<div class="aviso-erro"><p>O Título precisa ter entre 20 e 65 caracteres</p></div>`);
+        } 
+    } else contemErro--;
+ 
+        //confere erro na URL
+    let ehURL = urlCheck(urlImagemQuizz);
+    if(!ehURL) {
+        if(!url.classList.contains("tem-erro")){
+            url.classList.add("tem-erro");
+            url.insertAdjacentHTML("afterend",`<div class="aviso-erro"><p>O valor informado não é uma URL válida</p></div>`);
+        }
+    } else contemErro--;
+
+        //confere erro nas perguntas
+    if(qntPerguntasQuizz < 3) {
+        if(!pergunta.classList.contains("tem-erro")){
+            pergunta.classList.add("tem-erro");
+            pergunta.insertAdjacentHTML("afterend",`<div class="aviso-erro"><p>O quizz deve ter no mínimo 3 perguntas</p></div>`);
+        }
+    } else contemErro--;
+
+        //confere erro nos níveis
+    if(qntNiveisQuizz < 2) {
+        if(!nivel.classList.contains("tem-erro")){
+            nivel.classList.add("tem-erro");
+            nivel.insertAdjacentHTML("afterend",`<div class="aviso-erro"><p>O quizz deve ter no mínimo 2 níveis</p></div>`);
+        }
+    } else contemErro--;
+
+    if(contemErro !== 0) {
+        return;
+    }
+
+
+    
     //avança de página
     document.querySelector(".tela-tres-info-quizz").classList.add("escondido");
     document.querySelector(".tela-tres-perguntas-quizz").classList.remove("escondido");
-
-    //verifica se as entradas são válida
 }
 
 //Botão da segunda página de criar quizz, prossegue de página para níveis e computa os valores (tela 3.2)
@@ -93,33 +159,19 @@ function AcessarQuizz() {
 
 //Botão da primeira página de criar quizz. Prossegue de página e computa os valores (tela 3.4)
 function voltarHome() {
-    //Volta para home
-    document.querySelector(".tela-tres-sucesso-quizz").classList.add("escondido");
-    document.querySelector(".tela-tres").classList.add("escondido");
-    document.querySelector(".tela-um").classList.remove("escondido");
-
-    //Zera as variáveis de criação de quizz
-    tituloQuizz = "";
-    urlImagemQuizz = "";
-    qntPerguntasQuizz = "";
-    qntNiveisQuizz = "";
-    perguntaTexto = "";
-    perguntaCorFundo = "";
-    respostaCorreta = "";
-    respostaCorretaUrl = "";
-    respostaIncorreta1 = "";
-    respostaIncorretaUrl1 = "";
-    respostaIncorreta2 = "";
-    respostaIncorretaUrl2 = "";
-    respostaIncorreta3 = "";
-    respostaIncorretaUrl3 = "";
-    tituloNivel = "";
-    porcentagemAcerto = "";
-    urlImagemNivel = "";
-    descricaoNivel = "";
-
-    //Carrega os Meus quizzes
-
-    //Carrega os quizzes da comunidade
+    carregarPagina("home");
 }
 
+// Teste se a STRING é uma URL
+function urlCheck(str)
+{
+  regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(str))
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+}
